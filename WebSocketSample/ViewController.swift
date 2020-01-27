84c4a36cb54811cf5c17bf6d7d265e28e43f2c60
Vladyslav Vcherashnii <vladyslav.vcherashnii@gmail.com>
2020-01-27 11:42:04 -0500

    Initial Commit

---
+++ WebSocketSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebSocketSample
+//
+//  Created by Vladyslav Vcherashnii on 27.01.2020.
+//  Copyright © 2020 Vladyslav Vcherashnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
